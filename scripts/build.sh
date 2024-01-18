4a2f52944ac9808ae576588dd8a890f7c99cd05d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-01-18 11:06:08 -0500

    ci: change config file for release file upload

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env bash
 
 ls dist/
-rm dist/SHASUMS256.txt
+rm -rfv dist/*
+
+TAG=${RELEASE_VERSION:-canary}
 
 # Rename the binaries
-mv -v dist/gh-bofh-aarch64-apple-darwin dist/gh-bofh_${RELEASE_VERSION}_darwin-arm64
-mv -v dist/gh-bofh-aarch64-unknown-linux-gnu dist/gh-bofh_${RELEASE_VERSION}_linux-arm64
-mv -v dist/gh-bofh-i686-unknown-linux-gnu dist/gh-bofh_${RELEASE_VERSION}_linux-386
-mv -v dist/gh-bofh-x86_64-apple-darwin dist/gh-bofh_${RELEASE_VERSION}_darwin-amd64
-mv -v dist/gh-bofh-x86_64-unknown-linux-gnu dist/gh-bofh_${RELEASE_VERSION}_linux-amd64
-mv -v dist/gh-bofh-x86_64-pc-windows-gnu dist/gh-bofh_${RELEASE_VERSION}_windows-amd64.exe
-mv -v dist/gh-bofh-i686-pc-windows-gnu dist/gh-bofh_${RELEASE_VERSION}_windows-386.exe
+cp -vr artifacts/aarch64-apple-darwin/gh-bofh-aarch64-apple-darwin dist/gh-bofh_${TAG}_darwin-arm64
+cp -vr artifacts/aarch64-unknown-linux-gnu/gh-bofh-aarch64-unknown-linux-gnu dist/gh-bofh_${TAG}_linux-arm64
+cp -vr artifacts/i686-unknown-linux-gnu/gh-bofh-i686-unknown-linux-gnu dist/gh-bofh_${TAG}_linux-386
+cp -vr artifacts/x86_64-apple-darwin/gh-bofh-x86_64-apple-darwin dist/gh-bofh_${TAG}_darwin-amd64
+cp -vr artifacts/x86_64-unknown-linux-gnu/gh-bofh-x86_64-unknown-linux-gnu dist/gh-bofh_${TAG}_linux-amd64
+cp -vr artifacts/x86_64-pc-windows-gnu/gh-bofh-x86_64-pc-windows-gnu dist/gh-bofh_${TAG}_windows-amd64.exe
+cp -vr artifacts/i686-pc-windows-gnu/gh-bofh-i686-pc-windows-gnu dist/gh-bofh_${TAG}_windows-386.exe
 
 # Create the checksums
-shasum -a 256 dist/* > dist/SHASUMS256.txt
+shasum -a 256 dist/* > dist/SHA256SUMS.txt
 
 tree -a dist/
