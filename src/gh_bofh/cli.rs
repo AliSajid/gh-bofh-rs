51244c3501423bf8cf358c5b74d8c7bb1871102d
Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>
2024-09-17 11:41:38 -0400

    test: add tests to the cli module
    Signed-off-by: Ali Sajid Imami <395482+AliSajid@users.noreply.github.com>

--- src/gh_bofh/cli.rs
+++ src/gh_bofh/cli.rs
@@ -12,7 +12,7 @@ use clap::{
     ValueEnum,
 };
 
-#[derive(Debug, Clone, Copy, ValueEnum)]
+#[derive(Debug, Clone, Copy, ValueEnum, PartialEq, Eq)]
 pub enum ExcuseType {
     Classic,
     Modern,
@@ -53,4 +53,54 @@ pub struct Cli {
     /// Generates a more modern BOFH excuse.
     #[arg(short = 'm', long = "modern", group = "type")]
     pub modern: bool,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_default_classic_excuse() {
+        let args = Cli::parse_from(["test"]);
+        assert_eq!(args.excuse_type, ExcuseType::Classic);
+    }
+
+    #[test]
+    fn test_specify_classic_excuse() {
+        let args = Cli::parse_from(["test", "--type", "classic"]);
+        assert_eq!(args.excuse_type, ExcuseType::Classic);
+    }
+
+    #[test]
+    fn test_specify_modern_excuse() {
+        let args = Cli::parse_from(["--type", "modern"]);
+        assert_eq!(args.excuse_type, ExcuseType::Modern);
+    }
+
+    #[test]
+    fn test_short_flag_classic() {
+        let args = Cli::parse_from(["test", "-c"]);
+        assert!(args.classic);
+    }
+
+    #[test]
+    fn test_short_flag_modern() {
+        let args = Cli::parse_from(["test", "-m"]);
+        assert!(args.modern);
+        assert!(!args.classic);
+    }
+
+    #[test]
+    fn test_env_var_classic() {
+        std::env::set_var("EXCUSE_TYPE", "classic");
+        let args = Cli::parse_from(["test"]);
+        assert_eq!(args.excuse_type, ExcuseType::Classic);
+    }
+
+    #[test]
+    fn test_env_var_modern() {
+        std::env::set_var("EXCUSE_TYPE", "modern");
+        let args = Cli::parse_from(["test"]);
+        assert_eq!(args.excuse_type, ExcuseType::Modern);
+    }
 }
